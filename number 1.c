7c5415790793f40152ad60a69a9395559f7ba8b1
waple7 <91288621+waple7@users.noreply.github.com>
2021-10-21 19:39:43 +0300

    Update number 1.c

--- number 1.c
+++ number 1.c
@@ -3,7 +3,7 @@
 int main(){
     int n=8;
     int array[]={99,88,77,66,55,44,33,22,11};
-    printf("array : ");
+    printf("array values : ");
     for(n=0;n<=8;n++){
         printf( " %d ", array[n]);
     }
