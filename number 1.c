c1b61326bb53fd370fbe581799f9190b6c431d65
waple7 <91288621+waple7@users.noreply.github.com>
2021-10-21 17:55:01 +0300

    Add files via upload

---
+++ number 1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int n=8;
+    int array[]={99,88,77,66,55,44,33,22,11};
+    printf("array : ");
+    for(n=0;n<=8;n++){
+        printf( " %d ", array[n]);
+    }
+}
