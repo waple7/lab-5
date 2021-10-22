9871a7f4f2bf680433bb8b51881bec10b651b2c4
waple7 <91288621+waple7@users.noreply.github.com>
2021-10-22 09:42:02 +0300

    Update number_2.c

--- number_2.c
+++ number_2.c
@@ -1,27 +1,30 @@
 #include<stdio.h>
 
 int main() {
-    int array_1[2][2] = {{2, 5},
-                         {2, 2}};
+    int a1[2][2] = {2, 5, 2, 2};
+    int a2[2][2] = {1, 2, 0, 1};
+    int a3[2][2] = {0};
 
-    int array_2[2][2] = {{1, 2},
-                         {0, 1}};
+    for (int i = 0; i < 2; i++) {
 
-    int high_left;
-    int high_right;
-    int down_left;
-    int down_right;
+        for (int j = 0; j < 2; j++) {
 
-    high_left = (array_1[0][0]) * (array_2[0][0]) + (array_1[0][1]) * (array_2[1][0]);
-    high_right = (array_1[0][0]) * (array_2[0][1]) + (array_1[0][1]) * (array_2[1][1]);
-    down_left = (array_1[1][0]) * (array_2[0][0]) + (array_1[1][1]) * (array_2[1][0]);
-    down_right = (array_1[1][0]) * (array_2[0][1]) + (array_1[1][1]) * (array_2[1][1]);
+            for (int k = 0; k < 2; k++) {
 
-    printf("resulting matrix:\n");
-    printf("%d  %d\n", high_left, high_right);
-    printf("%d  %d", down_left, down_right);
+                a3[i][j] += a1[i][k] * a2[k][j];
+            }
+
+        }
+    }
+
+    for (int i = 0; i < 2; i++) {
+
+        for (int k = 0; k < 2; k++) {
+            
+            printf("%d", a3[i][k]);
+        }
+        printf("\n");
+    }
 
     return 0;
-
-
 }
