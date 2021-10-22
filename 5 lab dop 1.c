7599b1bf4aaef6ad84f78411289ef30d733db333
waple7 <91288621+waple7@users.noreply.github.com>
2021-10-22 09:51:15 +0300

    Add files via upload

---
+++ 5 lab dop 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    int a[2][2] = {2, 9,
+                   2, 6};
+
+    printf("%d%d", a[0][0], a[1][0]);
+    printf("\n");
+    printf("%d%d", a[0][1], a[1][1]);
+
+
+    return 0;
+}
