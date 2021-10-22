36d62cef266891b2e5dacffabee8722927caf70b
waple7 <91288621+waple7@users.noreply.github.com>
2021-10-22 09:01:07 +0300

    Update number_2.c

--- number_2.c
+++ number_2.c
@@ -1,22 +1,27 @@
 #include<stdio.h>
 
-int main(){
-    int array_1[2][2]={{2,5},{2,2}};//2 5
-                                    //2 2
-    int array_2[2][2]={{1,2},{0,1}};//1 2
-                                    //0 1
+int main() {
+    int array_1[2][2] = {{2, 5},
+                         {2, 2}};
+
+    int array_2[2][2] = {{1, 2},
+                         {0, 1}};
+
     int high_left;
     int high_right;
     int down_left;
     int down_right;
-    high_left= (array_1[0][0]) * (array_2[0][0])+(array_1[0][1])*(array_2[1][0]);
-    high_right= (array_1[0][0]) * (array_2[0][1]) + (array_1[0][1]) * (array_2[1][1]);
-    down_left= (array_1[1][0]) * (array_2[0][0]) + (array_1[1][1]) * (array_2[1][0]);
-    down_right= (array_1[1][0]) * (array_2[0][1]) + (array_1[1][1]) * (array_2[1][1]);
-    
+
+    high_left = (array_1[0][0]) * (array_2[0][0]) + (array_1[0][1]) * (array_2[1][0]);
+    high_right = (array_1[0][0]) * (array_2[0][1]) + (array_1[0][1]) * (array_2[1][1]);
+    down_left = (array_1[1][0]) * (array_2[0][0]) + (array_1[1][1]) * (array_2[1][0]);
+    down_right = (array_1[1][0]) * (array_2[0][1]) + (array_1[1][1]) * (array_2[1][1]);
+
     printf("resulting matrix:\n");
-    printf("%d  %d\n",high_left,high_right);
-    printf("%d  %d",down_left,down_right);
+    printf("%d  %d\n", high_left, high_right);
+    printf("%d  %d", down_left, down_right);
+
+    return 0;
 
 
 }
